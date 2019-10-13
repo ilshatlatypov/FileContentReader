9a7f6923399a81f7d889a5c11572162021544b7f
Ilshat Latypov <latypov.ilshat@gmail.com>
2019-10-13 18:43:30 +0300

    get file details

--- src/android/FileContentReader.java
+++ src/android/FileContentReader.java
@@ -1,9 +1,7 @@
 package cordova.plugin.file.content.reader;
 
-import android.app.Activity;
-import android.content.ClipData;
 import android.content.Context;
-import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 
 import org.apache.cordova.CallbackContext;
@@ -15,17 +13,19 @@ import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileContentReader extends CordovaPlugin {
 
     private static final String ACTION_READ_CONTENT = "readContent";
+    private static final String ACTION_GET_FILE_DETAILS = "getFileDetails";
+
+    private static final String FIELD_DISPLAY_NAME = "_display_name";
+    private static final String FIELD_SIZE = "_size";
+    private static final String FIELD_MIME_TYPE = "mime_type";
 
     private CallbackContext callback;
     private String contentUri;
@@ -37,6 +37,9 @@ public class FileContentReader extends CordovaPlugin {
 
         if (action.equals(ACTION_READ_CONTENT)) {
             readContent();
+            return true;
+        } else if (action.equals(ACTION_GET_FILE_DETAILS)) {
+            getFileDetails();
             return true;
         }
         return false;
@@ -87,6 +90,40 @@ public class FileContentReader extends CordovaPlugin {
                     os.close();
                 } catch (IOException e) {
                 }
+            }
+        }
+    }
+
+    private void getFileDetails() {
+        Context appContext = this.cordova.getActivity().getApplicationContext();
+        Uri uri = Uri.parse(this.contentUri);
+
+        String[] columns = new String[]{FIELD_DISPLAY_NAME, FIELD_SIZE, FIELD_MIME_TYPE};
+        Cursor cursor = null;
+
+        try {
+            cursor = appContext.getContentResolver()
+                    .query(uri, columns, null, null, null);
+
+            if (cursor != null && cursor.moveToFirst()) {
+                int titleColumnIndex = cursor.getColumnIndexOrThrow(FIELD_DISPLAY_NAME);
+                int sizeColumnIndex = cursor.getColumnIndexOrThrow(FIELD_SIZE);
+                int typeColumnIndex = cursor.getColumnIndexOrThrow(FIELD_MIME_TYPE);
+
+                Map<String, String> fileDetails = new HashMap<>();
+                fileDetails.put("title", cursor.getString(titleColumnIndex));
+                fileDetails.put("type", cursor.getString(typeColumnIndex));
+                fileDetails.put("size", String.valueOf(cursor.getLong(sizeColumnIndex)));
+                fileDetails.put("uri", this.contentUri);
+
+
+                this.callback.success(new JSONObject(fileDetails));
+            } else {
+                this.callback.error("Информация о файле недоступна");
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
             }
         }
     }
--- www/FileContentReader.js
+++ www/FileContentReader.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 module.exports.readContent = function (contentUri, success, error) {
     exec(success, error, 'FileContentReader', 'readContent', [contentUri]);
 };
+
+module.exports.getFileDetails = function (contentUri, success, error) {
+    exec(success, error, 'FileContentReader', 'getFileDetails', [contentUri]);
+};
